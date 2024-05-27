bc47b01b885baf11ace558799d6605204ac863ce
Char <akmeduri1@gmail.com>
2024-05-27 15:52:38 -0700

    Default Mod File

---
+++ src/main/java/net/charval/dragonaddons/DragonAddonsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.charval.dragonaddons;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DragonAddonsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
